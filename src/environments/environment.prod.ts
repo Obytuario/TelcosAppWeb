02063a8989a43818341af426ff8c61edf7185d08
arielb <arielb@digitalware.com.co>
2023-03-05 11:53:13 -0500

    Compilacion de archivos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8083/TelcosSoftApi/Api'
+  //apiUrl: 'http://localhost:8083/TelcosSoftApi'
+  apiUrl: 'https://localhost:7145'
 };
